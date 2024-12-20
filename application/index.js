9f78fbc172f5d41a063401887cf238f8edfb24c1
Kedar Bhandare <kedar@us.brainscale.com>
2024-12-20 13:29:56 +0530

    This app and infrastructure is created from Backstage.io

--- application/index.js
+++ application/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send("Hello from NodeJSApp!");
+  res.send("Hello, This app and infrastructure is created from Backstage.io. Hurray!!");
 });
 
 app.listen(port, () => {
